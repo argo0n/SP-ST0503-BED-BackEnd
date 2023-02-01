55df16da1be00b460850b8c6aa32f05788e9c3d5
Argon <argon@nogra.xyz>
2023-02-01 14:22:41 +0800

    Add search films by title method, Fix films by category method, Add films by film ID, add film categories method

---
+++ model/films.js
@@ -0,0 +1,156 @@
+/*
+Name        LAI YE QI
+Class       DIT/FT/1B/05
+Group       ??
+Admn        P2222189
+ */
+
+const db = require("./databaseConfig");
+
+
+module.exports = {
+    films_by_category: function (category_id, callback) {
+        var dbConn = db.getConnection();
+        dbConn.connect(function (err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                const sql = `
+                    SELECT
+                        fi.film_id, fi.title, ca.name, fi.rating, fi.release_year, fi.length as duration
+                    FROM
+                        film fi
+                            INNER JOIN film_category fica ON fi.film_id = fica.film_id
+                            INNER JOIN category ca on fica.category_id = ca.category_id
+                    WHERE
+                        ca.name = ?
+                `
+                dbConn.query(sql, [category_id], function (err, results) {
+                    dbConn.end();
+                    console.log(results)
+                    if (err) {
+                        return callback(err, null);
+                    } else {
+                        return callback(null, results);
+                    }
+                })
+            }
+        })
+    },
+
+    films_by_title: function (title, max_price, callback) {
+        var dbConn = db.getConnection();
+        dbConn.connect(function(err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                let sql = `
+                SELECT 
+                fi.film_id, fi.title, ca.name as category, fi.description, fi.release_year, la.name as language, fi.length, fi.rating, fi.special_features, fi.rental_rate
+                FROM film fi
+                INNER JOIN film_category fica on fi.film_id = fica.film_id
+                INNER JOIN category ca on fica.category_id = ca.category_id
+                INNER JOIN language la on fi.language_id = la.language_id
+                WHERE fi.title like ?
+                `
+                if (!isNaN(max_price) && max_price > 0) {
+                    sql += " AND fi.rental_rate < ?"
+                }
+                console.log(sql)
+                console.log(title)
+                // title = JSON.parse(title); // Included this to remove quotes
+                let param = `%${title}%`
+                console.log(param)
+                dbConn.query(sql, [param, max_price], function(err, results) {
+                    dbConn.end();
+                    if (err) {
+                        return callback(err, null);
+                    } else {
+                        return callback(null, results);
+                    }
+                })
+
+            }
+        })
+    },
+
+    film_by_id: function (id, callback) {
+        var dbConn = db.getConnection();
+        dbConn.connect(function(err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                const sql = `
+                SELECT 
+                fi.film_id, fi.title, ca.name as category, fi.description, fi.release_year, la.name as language, fi.length, fi.rating, fi.special_features, fi.rental_rate
+                FROM film fi
+                INNER JOIN film_category fica on fi.film_id = fica.film_id
+                INNER JOIN category ca on fica.category_id = ca.category_id
+                INNER JOIN language la on fi.language_id = la.language_id
+                WHERE fi.film_id = ?
+                `
+                dbConn.query(sql, [id], function(err, results) {
+                    if (err) {
+                        dbConn.end();
+                        return callback(err, null);
+                    } else {
+                        if (results.length === 0) {
+                            dbConn.end();
+                            return callback(null, null);
+                        } else {
+                            var film = results[0]
+                            var actor_sql = `
+                    SELECT ac.first_name, ac.last_name, fi.title 
+                    FROM actor ac
+                    INNER JOIN film_actor fa on ac.actor_id = fa.actor_id
+                    INNER JOIN film fi on fa.film_id = fi.film_id
+                    `
+                            dbConn.query(actor_sql, function(err, actor_results) {
+                                dbConn.end();
+                                if (err) {
+                                    return callback(err, null);
+                                } else {
+                                    film.actors = [];
+                                    for (var j = 0; j < actor_results.length; j++) {
+                                        var actor = actor_results[j];
+                                        if (actor.title === film.title) {
+                                            film.actors.push(`${actor.first_name} ${actor.last_name}`);
+                                        }
+                                    }
+                                    return callback(null, film);
+                                    }
+                                })
+                        }
+                    }
+                })
+            }
+        })
+    },
+
+    film_categories: function(callback) {
+        var dbConn = db.getConnection();
+        dbConn.connect(function(err) {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                const sql = `
+                SELECT 
+                category_id, name
+                FROM category
+                `
+                dbConn.query(sql, function(err, results) {
+                    dbConn.end();
+                    if (err) {
+                        return callback(err, null);
+                    } else {
+                        return callback(null, results);
+                    }
+                })
+            }
+        })
+    }
+}
